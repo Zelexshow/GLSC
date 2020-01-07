f0757200a2180e0b737f4738672e31abdf084029
ZelexHome <zelexshaw@163.com>
2020-01-07 18:11:02 +0800

    项目调试完成

--- gmall-pms/src/main/java/com/zelex/gmall/pms/GmallPmsApplication.java
+++ gmall-pms/src/main/java/com/zelex/gmall/pms/GmallPmsApplication.java
@@ -1,8 +1,21 @@
 package com.zelex.gmall.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 1、配置整合dubbo
+ * 2、配置整合mybatis plus
+ *
+ * logstash整合
+ * 1）导jar
+ * 2）导日志配置
+ * 3）在kibana里面建立好日志的索引，就可以可视化检索
+ */
+@EnableDubbo
+@MapperScan(basePackages = "com.zelex.gmall.pms.mapper")
 @SpringBootApplication
 public class GmallPmsApplication {
 
--- gmall-pms/src/test/java/com/zelex/gmall/pms/GmallPmsApplicationTests.java
+++ gmall-pms/src/test/java/com/zelex/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,31 @@
 package com.zelex.gmall.pms;
 
+import com.zelex.gmall.pms.entity.Brand;
+import com.zelex.gmall.pms.entity.Product;
+import com.zelex.gmall.pms.service.BrandService;
+import com.zelex.gmall.pms.service.ProductService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GmallPmsApplicationTests {
+    @Autowired
+    ProductService productService;
 
+    @Autowired
+    BrandService brandService;
     @Test
     void contextLoads() {
+//        Product byId = productService.getById(1);
+//        System.out.println(byId.getName());
+        //测试增删改在主库，查在从库
+//        Brand brand=new Brand();
+//        brand.setName("哈哈哈");
+//        brandService.save(brand);
+//        System.out.println("保存成功...");
+        Brand byId=brandService.getById(53);
+        System.out.println(byId.getName());
     }
 
 }
